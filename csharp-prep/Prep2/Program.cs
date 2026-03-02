e0b1844220fa4a47b54bb1c8ec2f62a46c34f2be
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 15:57:33 -0700

    Completed C# Prep 2 Core Requirement 3.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,41 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Obtain grade from user, convert to integer.
+        // Obtain grade from user, convert to integer
         Console.Write("What is you grade percentage? ");
         string gradeString = Console.ReadLine();
-        int grade = int.Parse(gradeString);
+        float grade = float.Parse(gradeString);
+
+        // Initialize gradeLetter and passFailString
+        string gradeLetter;
+        string passFailString;
 
         // Determine letter grade via if - else - else if funnel
 
         if (grade >= 90) // Grade A
         {
-            Console.WriteLine("Your grade: A");
-            Console.WriteLine("Congratulations! That is a passing grade.");
+            gradeLetter = "A";
+            passFailString = "Congratulations! That is a passing grade.";
         }
 
         else if (grade >= 80) // Grade B
         {
-            Console.WriteLine("Your grade: B");
-            Console.WriteLine("Congratulations! That is a passing grade.");
+            gradeLetter = "B";
+            passFailString = "Congratulations! That is a passing grade.";
         }
 
         else if (grade >= 70) // Grade C
         {
-            Console.WriteLine("Your grade: C");
-            Console.WriteLine("Congratulations! That is a passing grade.");
+            gradeLetter = "C";
+            passFailString = "Congratulations! That is a passing grade.";
         }
 
         else if (grade >= 60) // Grade D
         {
-            Console.WriteLine("Your grade: D");
-            Console.WriteLine("Unfortunately, this is not a passing grade.");
+            gradeLetter = "D";
+            passFailString = "Unfortunately, this is not a passing grade.";
         }
 
         else // Grade F
         {
-            Console.WriteLine("Your grade: F");
-            Console.WriteLine("Unfortunately, this is not a passing grade.");
+            gradeLetter = "F";
+            passFailString = "Unfortunately, this is not a passing grade.";
         }
+
+        // Print user letter grade and acknowledgement of pass or fail
+        Console.WriteLine($"Your grade: {gradeLetter}");
+        Console.WriteLine(passFailString);
     }
 }
