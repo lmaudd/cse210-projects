d50f7563404c7ba956e32e815ff879d1822c823d
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 15:12:34 -0700

    Wrote code for Prep 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,23 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        // Obtain First Name from user
+        Console.WriteLine(" ");
+        Console.WriteLine("What is your first name?");
+        string nameFirst = Console.ReadLine();
+
+        // Obtain Last Name from user
+        Console.WriteLine(" ");
+        Console.WriteLine("What is your last name?");
+        string nameLast = Console.ReadLine();
+
+        // Print User Name
+        // note that I found a guide for string interpolation at:
+        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+        Console.WriteLine(" ");
+        string printSentence = $"Your name is {nameLast}, {nameFirst} {nameLast}.";
+        Console.WriteLine(printSentence);
+        Console.WriteLine(" ");
     }
 }
