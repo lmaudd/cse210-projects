a775f63cf4798beff8efbd8af68045f4d7d03ecb
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 15:33:17 -0700

    Completed C# Prep 2 Core Requirement 1.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Obtain grade from user, convert to integer.
+        Console.Write("What is you grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        // Determine letter grade via if - else - else if funnel
+
+        if (grade >= 90) // Grade A
+        {
+            Console.WriteLine("Your grade: A");
+        }
+
+        else if (grade >= 80) // Grade B
+        {
+            Console.WriteLine("Your grade: B");
+        }
+
+        else if (grade >= 70) // Grade C
+        {
+            Console.WriteLine("Your grade: C");
+        }
+
+        else if (grade >= 60) // Grade D
+        {
+            Console.WriteLine("Your grade: D");
+        }
+
+        else // Grade F
+        {
+            Console.WriteLine("Your grade: F");
+        }
     }
 }
