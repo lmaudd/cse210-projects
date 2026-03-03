4587a8aa313fc91fab78096869b5d36323c538af
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 22:03:57 -0700

    Added code to main function to test Job class. Test successful.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Test your Job class with 2 jobs
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Consultant";
+        job1._startYear = "2003";
+        job1._endYear = "2017";
+        job1.DisplayInformation();
+
+        Job job2 = new Job();
+        job2._company = "The Ranch";
+        job2._jobTitle = "Farmer";
+        job2._startYear = "17";
+        job2._endYear = "60";
+        job2.DisplayInformation();
     }
 }
