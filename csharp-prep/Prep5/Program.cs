ca296a6520af3b5ef04a103bed26e9106425c997
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 20:06:12 -0700

    Updated C# Prep 5 to comply with instructions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,30 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Obtain user input.
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int numSquared = SquareNumber(number);
+        int birthYear = PromtUserBirthYear();
+        int userAge = UserAge(birthYear);
+        WriteToConsole(name, numSquared, userAge);
+    }
+
+    static void DisplayWelcome()
+    {
         Console.WriteLine(" ");
         Console.WriteLine("Welcome to the program!");
+    }
 
+    static string PromptUserName()
+    {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
+        return name;
+    }
 
+    static int PromptUserNumber()
+    {
         Console.Write("Please enter your favorite number: ");
         string favoriteNumber = Console.ReadLine();
+        int favoriteNumberInt = int.Parse(favoriteNumber);
+        return favoriteNumberInt;
+    }
 
+    static int PromtUserBirthYear()
+    {
         Console.Write("Please enter the year you were born: ");
         string dateOfBirth = Console.ReadLine();
-
-        // Calculations
-        int favoriteNumberInt = int.Parse(favoriteNumber);
-        int numSquared = favoriteNumberInt * favoriteNumberInt;
-
         int dateOfBirthInt = int.Parse(dateOfBirth);
-        int aegThisYear = 2026 - dateOfBirthInt;
+        return dateOfBirthInt;
+    }
 
-        // Write to console
+    static int SquareNumber(int number)
+    {
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+
+    static int UserAge(int birthYear)
+    {
+        int ageThisYear = 2026 - birthYear;
+        return ageThisYear;
+    }
+
+    static void WriteToConsole(string name, int numSquared, int userAge)
+    {
         Console.WriteLine($"{name}, the square of your number is {numSquared}.");
-        Console.WriteLine($"{name}, you will turn {aegThisYear} this year.");
+        Console.WriteLine($"{name}, you will turn {userAge} this year.");
         Console.WriteLine(" ");
-
     }
 }
