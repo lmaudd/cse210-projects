0ff4fb46b9f476c2a24fe3517c7719081d4afa53
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 17:02:20 -0700

    Worked on C# Prep 3 Core Req. 1.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Establish magic number
+        Console.WriteLine(" ");
+        Console.WriteLine("----------------------------------");
+        Console.Write("What is your magic number? "); // Ask question
+        string magicNumberInput = Console.ReadLine(); // Store answer as string
+        int magicNumber = int.Parse(magicNumberInput); // Store number as int
+        Console.WriteLine(" ");
+        
+        // Initialize loop condition
+        bool cont = true;
+
+        // Game Loop
+        while (cont == true)
+        {
+            // Establish guess
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            int guess = int.Parse(guessInput);
+
+            // Victory condition
+            if (guess == magicNumber)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Correct!");
+                Console.WriteLine("----------------------------------");
+                cont = false;
+            }
+
+            // Too high
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            // Too low
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
