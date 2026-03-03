ff971bb454a18470aba77574add7709506a160d2
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 22:10:41 -0700

    Created Jobs.cs and added responsibilities.

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Resume
+{
+    // Define member variables (responsibilities) for Resume class
+    public string _name = "";
+    public List<string> _jobs = new List<string>();
+}
