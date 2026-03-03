9ee992cbd6a24329f805a31cb11ab2f7be1fd7c5
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-02 19:32:24 -0700

    C# Prep 5: Done.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Obtain user input
+        Console.WriteLine(" ");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumber = Console.ReadLine();
+
+        Console.Write("Please enter the year you were born: ");
+        string dateOfBirth = Console.ReadLine();
+
+        // Calculations
+        int favoriteNumberInt = int.Parse(favoriteNumber);
+        int numSquared = favoriteNumberInt * favoriteNumberInt;
+
+        int dateOfBirthInt = int.Parse(dateOfBirth);
+        int aegThisYear = 2026 - dateOfBirthInt;
+
+        // Write to console
+        Console.WriteLine($"{name}, the square of your number is {numSquared}.");
+        Console.WriteLine($"{name}, you will turn {aegThisYear} this year.");
+        Console.WriteLine(" ");
+
     }
 }
