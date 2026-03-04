02280cb22ef05476fa35af42cd2b2dcdb6327d13
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-03 19:39:11 -0700

    Outlined Entry.cs

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Entry
+{
+    // Define member variables (responsibilities)
+    public string _prompt = "";
+    public string _date = "";
+    public string _answer = "";
+
+    // Define methods
+    public void PromptUser()
+    {
+   
+    }
+
+    public void Display()
+    {
+   
+    }
+}
