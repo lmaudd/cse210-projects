01a1f9cad3acdeafe493f1e93a56a2bcdde1ff16
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-03 20:39:10 -0700

    Done w/ Prompt.cs (for now)

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-       // List Practice
-       List<string> myList = new List<String> { "C#", "Java", "Javascript" };
-       
-        foreach (string listElement in myList) 
-        {
-            Console.WriteLine(listElement);
-        }
+       // Empty
     }
 }
--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -1,20 +1,34 @@
-// using System;
-// using System.Reflection;
+using System;
+using System.Reflection;
 
-// public class Prompts
-// {
-//     // Define member variables (responsibilities)
-//     // ResourceAttributes: https://www.geeksforgeeks.org/c-sharp/list-implementation-in-c-sharp/
-//     // public List<string> _prompts = new List<string> { "1", "2" };
+public class Prompts
+{
+    // Define member variables (responsibilities)
+    // Resource: https://www.geeksforgeeks.org/c-sharp/list-implementation-in-c-sharp/
+    public List<string> _prompts = new List<string> 
+    {
+        "Prompt #1 placeholder", 
+        "Prompt #2 placeholder",
+        "Prompt #3 placeholder",
+        "Prompt #4 placeholder",
+        "Prompt #5 placeholder",
+        "Prompt #7 placeholder",
+        "Prompt #8 placeholder",
+        "Prompt #9 placeholder",
+        "Prompt #10 placeholder"
+    };
 
-//     // Define methods
-//     public void PromptSelection()
-//     {
-   
-//     }
+    // Define methods
+    public string PromptSelection()
+    {
+        Random randomGenerator = new Random();
+        int index = randomGenerator.Next(9); // Randomly select from indices 0-9
+        string chosenPrompt = _prompts[index]; // Store corresponding prompt
+        return chosenPrompt;
+    }
 
-//     public void AddPrompt()
-//     {
-   
-//     }
-// }
+    public void AddPrompt(string newPrompt)
+    {
+        _prompts.Add(newPrompt);
+    }
+}
