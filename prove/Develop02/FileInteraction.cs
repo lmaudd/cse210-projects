de8ce7e79fd2e2400bcfefd8baafc5be1bd95199
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-03 19:43:18 -0700

    Outined FileInteractions.cs

---
+++ prove/Develop02/FileInteraction.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class FileInteractions
+{
+    // Define member variables (responsibilities)
+    public string _name = "";
+    public string _pin = "";
+
+    // Define methods
+    public void WriteFile()
+    {
+   
+    }
+
+    public void RetrieveFile()
+    {
+   
+    }
+}
--- prove/Develop02/Prompts.cs
+++ prove/Develop02/Prompts.cs
@@ -6,11 +6,6 @@ public class Prompts
     public List<string> _prompts = new List<String>();
 
     // Define methods
-    public void PromptUser()
-    {
-   
-    }
-
     public void PromptSelection()
     {
    
