317a2560a5ada7ad0d4f836f585844522f62bc2e
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-03 19:41:15 -0700

    Outlined Prompts.cs

---
+++ prove/Develop02/Prompts.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Prompts
+{
+    // Define member variables (responsibilities)
+    public List<string> _prompts = new List<String>();
+
+    // Define methods
+    public void PromptUser()
+    {
+   
+    }
+
+    public void PromptSelection()
+    {
+   
+    }
+
+    public void AddPrompt()
+    {
+   
+    }
+}
