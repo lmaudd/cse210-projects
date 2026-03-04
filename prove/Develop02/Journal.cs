cc388ee35147b940de7775c28423c3d75ccab822
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-03 19:36:36 -0700

    Outlined Journal.cs

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Journal
+{
+    // Define member variables (responsibilities)
+    public List<Entry> _entries = new List<Entry>();
+
+    // Define methods
+    public void FormString()
+    {
+   
+    }
+
+    public void Display()
+    {
+   
+    }
+
+    public void DisplayAll()
+    {
+   
+    }
+}
