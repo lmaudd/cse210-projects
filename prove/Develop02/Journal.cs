75570c47a4c0dfe6a5200ce8f167d051e79e3cef
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-07 17:51:55 -0700

    Journal.cs final polish

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,18 +1,27 @@
 using System;
 using System.IO;
 
+// File: Journal.cs
+//
+// Class Purpose: This defined the journal. This is the only class that
+// interfaces directly with the main program. It houses the instances of
+// Entry and Prompts. I want to finish implementing the add prompt feature,
+// but I would have to do so on the same instance that is used in GetEntry,
+// and I am unsure how to fully implement that. 
+
 public class Journal
 {
-    // Define member variables (responsibilities)
+    // Define responsibility to store all of the entries in our journal
     public List<Entry> _entries = new List<Entry>();
 
-    // Get Date
+    // Use DateTime to get/format the current date
     static DateTime time = DateTime.Now; // Get current date from datetime
     static string date = time.ToShortDateString(); // date -> string as "mm/dd/yyyy"
 
     // Define methods
     public void GetEntry()
     {
+        // This prompts the user, saves their answer, and stores it as an entry
         Prompts prompt = new Prompts();
         string newPrompt = prompt.PromptSelection();
         Console.WriteLine(" ");
@@ -25,6 +34,7 @@ public class Journal
 
     public void DisplayAll()
     {
+        // This prints all entries at once (and handels formatting)
         Console.WriteLine(" ");
         Console.WriteLine("All Journal Entries");
         foreach (Entry item in _entries)
@@ -36,12 +46,15 @@ public class Journal
 
     public void Save()
     {
+        // This saves your current entries as a file.
+        // It doesn't just append them to an existing file.
+        // It also requires you to set a pin which will be used to gain access upon load.
         Console.WriteLine(" ");
         Console.WriteLine("Saving Journal...");
         Console.Write("  - Please choose a filename (.txt): ");
-        string fileName = Console.ReadLine(); 
+        string fileName = Console.ReadLine(); // Save filename
         Console.Write("  - Please set a pin (4#): ");
-        string pin = Console.ReadLine();
+        string pin = Console.ReadLine(); // Save pin
         using (StreamWriter outputFile = new StreamWriter(fileName))
         {
             outputFile.WriteLine(pin); // Begin by saving pin
@@ -56,6 +69,9 @@ public class Journal
 
     public void Load()
     {   
+        // This loads the context of a previously saved .txt journal file
+        // inot our current journal's entry list. It requires you to input
+        // the correct pin to gain access. 
         Console.WriteLine(" ");
         Console.WriteLine("Loading Journal...");
         Console.Write("  - What is your file named? ");
@@ -64,12 +80,14 @@ public class Journal
         string pin = Console.ReadLine();
         string[] lines = System.IO.File.ReadAllLines(fileName);
 
-        if (lines.First() == pin)
+        if (lines.First() == pin) // Only proceed if the pin is correct
         {
-            foreach (string line in lines)
+            foreach (string line in lines) // Iterate through the whole file
             {
-                if (line != lines.First())
+                if (line != lines.First()) // Skip the first line (I found .First() in stack overflow)
                 {
+                    // Sepearate a line of text by "~" and store its information in an Entry, 
+                    // then add that Entry to our list of entries.
                     string[] parts = line.Split("~");
                     string date = parts[0];
                     string prompt = parts[1];
@@ -81,7 +99,6 @@ public class Journal
             }
             Console.WriteLine("  - Load successful.");
         }
-
             else
         {
             Console.WriteLine("  - Incorrect password. Load Failed.");
