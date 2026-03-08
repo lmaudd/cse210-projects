b6fb27bda872d1e1efa5c0a88e7e2f157536bf7a
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-07 17:34:12 -0700

    Entry.cs Final polish

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,17 +1,22 @@
 using System;
-using System.Dynamic;
+
+// File: Entry.cs
+//
+// Class Purpose: Define "Entry" class, which is sort of like a compound 
+// list to hold the date, prompt, and answer for each journal entry.
 
 public class Entry
 {
-    // Define member variables (responsibilities)
-    public string _date;
-    public string _prompt;
-    public string _answer;
+    // Define responsibilities to store journal entry information
+    public string _date;   // String to store current date (mm/dd/yyyy)
+    public string _prompt; // String to store prompt
+    public string _answer; // String to store answer
 
-    // Define methods
+    // Define method method to receive input and store in above variables
     public void StoreInformation(string date, string prompt, string answer)
     {
-        _prompt =  prompt;
+        // Store function parameters in the class responsibilites
+        _prompt = prompt;
         _answer = answer;
         _date = date;
     }
