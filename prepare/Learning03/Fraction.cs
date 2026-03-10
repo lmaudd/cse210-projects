d190a3df6d6f1acc70b3c0d55fd5f419d39ebf3e
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-10 13:09:48 -0600

    Create Fraction.cs file. Outline file attributes, constructors, getters/setters, and methods.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,56 @@
+using System;
+
+public class Fraction
+{
+    // Define class attributes
+    private int _top;
+    private int _bottom;
+
+    // Define class constructors
+    Fraction()
+    {
+        
+    }
+
+    Fraction(int wholeNumber)
+    {
+        
+    }
+
+    Fraction(int top, int bottom)
+    {
+        
+    }
+
+    // Define class getters and setters
+    GetTop()
+    {
+
+    }
+
+    SetTop(int top)
+    {
+
+    }
+
+    GetBottom()
+    {
+
+    }
+
+    SetBottom(int bottom)
+    {
+        
+    }
+
+    // Define class methods
+    GetFractionString()
+    {
+        
+    }
+    
+    GetDecimalValue()
+    {
+    
+    }
+}
