3493c377182693c56fae8acd3efd7d4c86154bce
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-12 12:20:21 -0600

    Worked on Words.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,8 +1,43 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
-class Words
+class Word
 {
     // Class attributes
     private string _word;
     private string _hiddenWord;
+
+    // Constuctor
+    public Word(string word)
+    {
+        _word = word;
+        _hiddenWord = HideWord(word);
+    }
+
+    // Class Methods
+    private string HideWord(string word)
+    {
+        int wordLength = word.Length+1;
+        string hiddenWord = "";
+
+        for (int i = 1; i < wordLength; i++)
+        {
+            hiddenWord += "_";
+        }
+
+        return hiddenWord;
+    }
+
+    public string GetWord(bool maskStatus=false) // mask true = _hiddenWord
+    {
+        if (maskStatus == false)
+        {
+            return _word;
+        }
+
+        else
+        {
+            return _hiddenWord;
+        }
+    }
 }
