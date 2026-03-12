e11cf6e07ba3a4f9490be08a571235f4d9926da5
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-12 12:30:26 -0600

    Worked on Reference.cs

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -8,4 +8,38 @@ class Reference
     private string _verse;
     private string _endVerse;
 
+     // Class constructors
+     public Reference(string book, string chapter, string verse)
+     {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = "";
+     }
+
+    public Reference(string book, string chapter, string verse, string endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    // Class methods
+    public string GetReference()
+    {
+        string reference = "";
+
+        if (_endVerse == "")
+        {
+            reference = $"{_book} {_chapter}:{_verse}";
+        }
+
+        else
+        {
+            reference = $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+
+        return reference;
+    }
 }
