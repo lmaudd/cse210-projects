66537f6cf38cbd2c7d007fe35e4e5235e5cc58d2
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-12 13:13:05 -0600

    Worked on Scripture.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+        Scripture scripture = new Scripture("hi", "hi");
+
+
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.HideWord();
+        scripture.Print();
+        Console.WriteLine("------------------");
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,9 +1,69 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 class Scripture
 {
     // Class attributes
     private Reference _reference;
-    private List<Words> _scripture;
+    private List<Word> _scripture;
     private List<int> _hiddenVerses;
+
+    // Class constructor
+    public Scripture(string scripture, string reference)
+    {
+        // _reference = GenerateReferenceList(reference);
+        // _scripture = GenerateWordList(scripture);
+        _hiddenVerses = new List<int>();
+
+    }
+
+    // Class set-up methods
+    // private List<Word> GenerateWordList(string scripture)
+    // {
+    
+    // }
+
+    // private Reference GenerateReferenceList(string reference)
+    // {
+
+    // }
+
+    // Class methods
+    public void HideWord()
+    // https://stackoverflow.com/questions/17381332/how-to-tell-if-a-list-does-not-contain-an-element
+    {
+        int length = 5;//_scripture.Count();
+        Random random = new Random();
+        int newIndex = -1;
+
+        while (true)
+        {
+            if (_hiddenVerses.Count() == length)//_scripture.Count())
+            {
+                break;
+            }
+            
+            newIndex = random.Next(length);
+
+            if (!_hiddenVerses.Contains(newIndex))
+            {
+                _hiddenVerses.Add(newIndex);
+                break;
+            }
+        }
+    }
+
+    public void Print()
+    {
+        foreach (int number in _hiddenVerses)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    public void DisplayScripture()
+    {
+
+    }
 }
