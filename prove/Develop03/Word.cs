2242df62d02afe6672c07db338e473d58c58101c
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-16 14:23:42 -0600

    Polish Words.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -12,39 +12,42 @@ class Word
     // Constuctor /////////////////////////////////////////////////////
     public Word(string word)
     {
-        _word = word;
-        _hiddenWord = HideWord(word);
-        _maskStatus = false;
+        _word = word;                  // Preload _word with string
+        _hiddenWord = HideWord(word);  // Preload _hiddenWord with string replaced by '_'
+        _maskStatus = false;           // Preload _maskStatus to false for now
     }
 
     // Class Methods ///////////////////////////////////////////////////
     private string HideWord(string word)
     {
-        string hiddenWord = "";
+        string hiddenWord = "";  // Initialize this and populate later
 
-        if (_word.Contains(',')) // word contains comma
+        // Account for words that might be followed by a comma, for example
+        // so that _hiddenWord goes from "word," to "____,"
+        if (_word.Contains(',')) 
         {
             int wordLength = word.Length;
             for (int i = 1; i < wordLength; i++)
             {
                 hiddenWord += "_";
             }
-
             hiddenWord += ",";
         }
 
-         else if (_word.Contains('.')) // word contains period
+        // Account for words that might be followed by a period, for example
+        // so that _hiddenWord goes from "word." to "____."
+        else if (_word.Contains('.')) 
         {
             int wordLength = word.Length;
             for (int i = 1; i < wordLength; i++)
             {
                 hiddenWord += "_";
             }
-
             hiddenWord += ".";
         }
 
-        else // Word contains neither comma nor period
+        // Regular word handling (no punctuation)
+        else
         {
             int wordLength = word.Length+1;
             for (int i = 1; i < wordLength; i++)
@@ -56,22 +59,26 @@ class Word
     }
 
     public bool GetMaskStatus()
-    {
+    {   
+        // Returns the mask's current status
         return _maskStatus;
     }
 
     public void SetMaskStatus(bool newStatus)
     {
+        // Changes the mask's status to new boolean
         _maskStatus = newStatus;
     }
 
     public string GetWord()
     {
+        // If mask is off, return regular _word
         if (_maskStatus == false)
         {
             return _word;
         }
 
+        // If mask is on, return _hiddenWord
         else
         {
             return _hiddenWord;
