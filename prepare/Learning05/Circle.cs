41ffd61361fe0cd32850a329016c244d7a9b3ab4
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-24 10:45:57 -0600

    Wrote and tested Circle.cs, it works.

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -1,6 +1,17 @@
-// using System;
+using System;
 
-// class Circle : Shape
-// {
+class Circle : Shape
+{
+    double _radius;
 
-// }
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        double pi = 3.1415926535;
+        return pi * _radius * _radius;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,9 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Square sq = new Square("blue", 2.0);
-        string color = sq.GetColor();
-        double area = sq.GetArea();
+        Circle crc = new Circle("Pink", 1.59576912161);
+        string color = crc.GetColor();
+        double area = crc.GetArea();
         Console.WriteLine($"\nTesting Square Class:\n- color = {color}\n- area = {area}\n");
     }
 }
