4eab340ebb5389e98b8563cca88d342f6b599fda
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-24 10:30:46 -0600

    Wrote Shape.cs code.

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,6 +1,26 @@
 using System;
 
-class Shape
+public class Shape
 {
+    string _color;
 
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return -1;
+    }
 }
