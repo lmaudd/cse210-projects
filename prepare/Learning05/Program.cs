8eb699ef6d3d4ad467fe500c556e26f346f6d380
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-24 10:58:03 -0600

    Wrote code's main implementation and got expected results. Assigment complete.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,9 +5,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Rectangle rc = new Rectangle("Black", 2, 4);
-        string color = rc.GetColor();
-        double area = rc.GetArea();
-        Console.WriteLine($"\nTesting Rectangle Class:\n- color = {color}\n- area = {area}\n");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Rectangle("Black", 2, 4) );
+        shapes.Add(new Square("Pink", 3));
+        shapes.Add(new Circle("Blue", 5));
+        shapes.Add(new Rectangle("Black", 1, 2));
+        shapes.Add(new Square("Yellow", 2));
+        shapes.Add(new Circle("Green", 10));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color={color},  Area={area}");
+        }
     }
 }
