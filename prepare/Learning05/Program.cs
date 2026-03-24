3fd10fabd7e24e32103dfd5771e37d2793144f8e
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-24 10:39:57 -0600

    Wrote code for Square.cs and tested. Code works as expected.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square sq = new Square("blue", 2.0);
+        string color = sq.GetColor();
+        double area = sq.GetArea();
+        Console.WriteLine($"\nTesting Square Class:\n- color = {color}\n- area = {area}\n");
     }
 }
