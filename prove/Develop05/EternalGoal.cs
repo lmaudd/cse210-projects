5764feb4fe578dd7149a5a781ea0d41a5ac643e9
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-26 11:44:46 -0600

    Outlined EternalGoal.cs

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,6 +1,12 @@
 using System;
 
-class EternalGoal
+public class EternalGoal : Goal
 {
+    public EternalGoal(string goal) : base(goal){}
 
+    public override int RecordEvent()
+    {
+        // Code here...
+        return 1;
+    }
 }
