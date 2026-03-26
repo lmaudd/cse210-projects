3a86abcfc3614717c18b45e361d1058c2f9db02e
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-26 13:17:18 -0600

    Worked on EternalGoal.cs

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -2,20 +2,24 @@ using System;
 
 public class EternalGoal : Goal
 {
+    // Member Variables
     private int _timesCompleted;
 
+    // Constructor 
     public EternalGoal(string goal, int pointValue) : base(goal, pointValue)
     {
         _timesCompleted = 0;
     }
 
+    // Methods
     public override void RecordEvent()
     {
         _timesCompleted++;
     }
+    
     public override int GetTotalPoints()
     {
-        // Code here ...
-        return 1;
+        int completionPoints = GetCompletionPoints();
+        return completionPoints * _timesCompleted;
     }
 }
