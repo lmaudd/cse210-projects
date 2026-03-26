1f249ba52bf61ef70dd03e83da7231c87bced95c
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-26 11:39:12 -0600

    Write AbstractGoal.cs

--- prove/Develop05/AbstractGoal.cs
+++ prove/Develop05/AbstractGoal.cs
@@ -1,6 +1,27 @@
 using System;
 
-class AbstractGoal
+public abstract class Goal
 {
+    private string _goal;
+    private int _pointValue;
+    private bool _goalStatus;
 
+    public Goal(string goal)
+    {
+        _goal = goal;
+        _pointValue = -1;
+        _goalStatus = false;
+    }
+
+    public void SetStatus(bool newStatus)
+    {
+        _goalStatus = newStatus;
+    }
+
+    public void SetPoints(int newPoints)
+    {
+        _pointValue = newPoints;
+    }
+
+    public abstract int RecordEvent();
 }
