23f0b295188523cfef6159ff7c0962ba8eb5c0ec
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-26 13:20:19 -0600

    Tested EternalGoal.cs and got expected results

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,15 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Test SimpleGoal
+        // string goal = "Drink more water.";
+        // int points = 50;
+        // SimpleGoal sg = new SimpleGoal(goal, points);
+
+        // int tp1 = sg.GetTotalPoints();
+        // sg.RecordEvent();
+        // int tp2 = sg.GetTotalPoints();
+
+        // Console.WriteLine(tp1);
+        // Console.WriteLine(tp2);
+
+        // Test EternalGoal
         string goal = "Drink more water.";
         int points = 50;
-        SimpleGoal sg = new SimpleGoal(goal, points);
+        EternalGoal eg = new EternalGoal(goal, points);
 
-        int tp1 = sg.GetTotalPoints();
-        sg.RecordEvent();
-        int tp2 = sg.GetTotalPoints();
+        int tp1 = eg.GetTotalPoints();
+        eg.RecordEvent();
+        int tp2 = eg.GetTotalPoints();
+        eg.RecordEvent();
+        eg.RecordEvent();
+        eg.RecordEvent();
+        int tp3 = eg.GetTotalPoints();
 
         Console.WriteLine(tp1);
         Console.WriteLine(tp2);
+        Console.WriteLine(tp3);
     }
 }
