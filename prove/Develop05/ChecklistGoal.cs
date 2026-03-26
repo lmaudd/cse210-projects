a509a3aab6c8652db584e6aa91866adc1ae71044
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-26 11:48:27 -0600

    Outline ChecklistGoal.cs

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,6 +1,21 @@
 using System;
 
-class ChecklistGoal
+public class ChecklistGoal : Goal
 {
+    private int _bonusPoints;
+    private int _timesCompleted;
+    private int _goalCompleted;
 
+    public ChecklistGoal(int bonusPoints, int timesCompleted, int goalCompleted, string goal) : base(goal)
+    {
+        _bonusPoints = bonusPoints;
+        _timesCompleted = timesCompleted;
+        _goalCompleted = goalCompleted;
+    }
+
+    public override int RecordEvent()
+    {
+        // Code here...
+        return 1;
+    }
 }
