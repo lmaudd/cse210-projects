6a8aa571b7bf4b0b23745f30c551358c2a8b0cd2
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-26 13:13:24 -0600

    Test of SimpleGoal.cs confirms expected results.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        string goal = "Drink more water.";
+        int points = 50;
+        SimpleGoal sg = new SimpleGoal(goal, points);
+
+        int tp1 = sg.GetTotalPoints();
+        sg.RecordEvent();
+        int tp2 = sg.GetTotalPoints();
+
+        Console.WriteLine(tp1);
+        Console.WriteLine(tp2);
     }
 }
