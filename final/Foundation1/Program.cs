57ac3107ac9d76513c026e88bc8a0e50c6eb4b0c
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-03-31 14:35:31 -0600

    Create 4 videos.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,6 +7,87 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Youtube Video 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        string video1Title = "C# Guide Part 1";
+        string video1Author = "C# Man";
+        string video1Length = "23 minutes";
 
+        string video1Comment1Text = "Great video!";
+        string video1Comment2Text = "Can you explain how you instantiated the class at the 10 min mark? Thanks!";
+        string video1Comment3Text = "Stupid video!!!";
+
+        string video1Comment1Name = "Mark H.";
+        string video1Comment2Name = "Jenny O.";
+        string video1Comment3Name = "Samuel B.";
+
+        Comment video1Comment1 = new Comment(video1Comment1Text, video1Comment1Name);
+        Comment video1Comment2 = new Comment(video1Comment2Text, video1Comment2Name);
+        Comment video1Comment3 = new Comment(video1Comment3Text, video1Comment3Name);
+        List<Comment> video1Comments = new List<Comment> {video1Comment1, video1Comment2, video1Comment3};
+
+        Video youtubeVideo1 = new Video(video1Title, video1Author, video1Length, video1Comments);
+
+        // Youtube Video 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        string video2Title = "Astrocraft Beginners Guide";
+        string video2Author = "Astrocrafter17";
+        string video2Length = "11 min";
+
+        string video2Comment1Text = "Wow cool game!";
+        string video2Comment2Text = "Anyone else thing this video is too short?";
+        string video2Comment3Text = "Here first!";
+        string video2Comment4Text = "Lame!";
+
+        string video2Comment1Name = "Jeff N..";
+        string video2Comment2Name = "Henry L.";
+        string video2Comment3Name = "Sherry F.";
+        string video2Comment4Name = "Sharkboy";
+
+        Comment video2Comment1 = new Comment(video2Comment1Text, video2Comment1Name);
+        Comment video2Comment2 = new Comment(video2Comment2Text, video2Comment2Name);
+        Comment video2Comment3 = new Comment(video2Comment3Text, video2Comment3Name);
+        Comment video2Comment4 = new Comment(video2Comment4Text, video2Comment4Name);
+        List<Comment> video2Comments = new List<Comment> {video2Comment1, video2Comment2, video2Comment3, video2Comment4};
+
+        Video youtubeVideo2 = new Video(video2Title, video2Author, video2Length, video2Comments);
+
+        // Youtube Video 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        string video3Title = "Shower Head Replacement Guide";
+        string video3Author = "DIY Girl";
+        string video3Length = "17 minutes";
+
+        string video3Comment1Text = "Cool video!";
+        string video3Comment2Text = "Hmmm idk about this.";
+        string video3Comment3Text = "Delete ur channel!!!!!";
+
+        string video3Comment1Name = "Louis B.";
+        string video3Comment2Name = "Louis B.";
+        string video3Comment3Name = "Wendy W.";
+
+        Comment video3Comment1 = new Comment(video3Comment1Text, video3Comment1Name);
+        Comment video3Comment2 = new Comment(video3Comment2Text, video3Comment2Name);
+        Comment video3Comment3 = new Comment(video3Comment3Text, video3Comment3Name);
+        List<Comment> video3Comments = new List<Comment> {video3Comment1, video3Comment2, video3Comment3};
+
+        Video youtubeVideo3 = new Video(video3Title, video3Author, video3Length, video3Comments);
+
+        // Youtube Video 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        string video4Title = "How to run a sub-5 mile.";
+        string video4Author = "Running Blog";
+        string video4Length = "8 minutes";
+
+        string video4Comment1Text = "Won't work!";
+        string video4Comment2Text = "This is decent.";
+        string video4Comment3Text = "Cool!";
+
+        string video4Comment1Name = "Jean B.";
+        string video4Comment2Name = "Sarah S.";
+        string video4Comment3Name = "Ben W.";
+
+        Comment video4Comment1 = new Comment(video4Comment1Text, video4Comment1Name);
+        Comment video4Comment2 = new Comment(video4Comment2Text, video4Comment2Name);
+        Comment video4Comment3 = new Comment(video4Comment3Text, video4Comment3Name);
+        List<Comment> video4Comments = new List<Comment> {video4Comment1, video4Comment2, video4Comment3};
+
+        Video youtubeVideo4 = new Video(video4Title, video4Author, video4Length, video4Comments);
     }
 }
