9280e54bfad3384d6bb2aa2800db262c7d638d2b
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-04-06 01:02:24 -0600

    Finish outlining Order Class

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -2,5 +2,37 @@ using System;
 
 public class Order
 {
+    // Attributes
+    private Customer _customer;
+    private List<Product> _products;
 
+    // Constructor 
+    public Order(Customer customer, List<Product> products)
+    {
+        // construct here
+        // ask if i should instantiate in constructor or in main (for now I will assume main since it will be easier)
+        _customer = customer;
+        _products = products;
+    }
+
+    // Methods
+    private float GetShippingCost()
+    {
+        
+    }
+
+    public float GetTotalCost()
+    {
+        
+    }
+
+    public string GetShippingLabelString()
+    {
+        
+    }
+
+    public string GetPackingLabelString()
+    {
+        
+    }
 }
