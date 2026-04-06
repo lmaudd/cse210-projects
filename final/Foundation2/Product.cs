5a1c92219963751451c22d36f4c9108fcde18c19
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-04-06 00:44:19 -0600

    Outline Product class

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,6 +1,26 @@
 using System;
+using System.Dynamic;
 
 public class Product
 {
+    // Attribute
+    private string _name;
+    private string _productID;
+    private float _unitCost;
+    private int _quantity; 
 
+    // Constructor
+    public Product(string name, string productID, float unitCost, int quantity)
+    {
+        _name = name;
+        _productID = productID;
+        _unitCost = unitCost;
+        _quantity = quantity;
+    }
+
+    // Methods
+    public float GetTotalCost()
+    {
+        
+    }
 }
