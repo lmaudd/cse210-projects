cb732bb2ff5b55e03f4dcd42c82a006613829ee8
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-04-09 01:49:41 -0600

    Main program finished. Running and biking work, somehow Swimming.cs returned infinity minutes per mile...

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,25 +6,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create list
+        List<Activity> exercises = new List<Activity>{};
+
         // Instantiate Running
         string rdate = "03 Nov 2022";
         float rlength = 30.0F;
         float rdistance = 3.0F;
         Running running = new Running(rdate, rlength, rdistance);
-        string rsummary = running.GetSummary();
-        // Console.WriteLine(rsummary);
+        exercises.Add(running);
 
         // Instantiate Biking
-        string bdate = "03 Nov 2022";
-        float blength = 30.0F;
-        float bdistance = 6.0F;
+        string bdate = "07 Oct 2025";
+        float blength = 20.0F;
+        float bdistance = 9.0F;
         Biking biking = new Biking(bdate, blength, bdistance);
-        string bsummary = biking.GetSummary();
-        // Console.WriteLine(bsummary);
-
+        exercises.Add(biking);
 
         // Instantiate Swimming
+        string sdate = "23 Sept 2029";
+        float slength = 45.0F;
+        int slapCount = 7;
+        Swimming swimming = new Swimming(sdate, slength, slapCount);
+        exercises.Add(swimming);
 
-
+        // Iterate through list
+        foreach (Activity act in exercises)
+        {
+            string sum = act.GetSummary();
+            Console.WriteLine($"\n{sum}\n");
+        }
     }
 }
