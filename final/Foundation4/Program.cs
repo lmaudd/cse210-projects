18e4b05aecdaeaae76723beb3cbe208918e549a7
lmaudd <140021591+lmaudd@users.noreply.github.com>
2026-04-09 01:35:19 -0600

    Running instantiation works

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,3 +1,5 @@
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+
 using System;
 
 class Program
@@ -5,7 +7,12 @@ class Program
     static void Main(string[] args)
     {
         // Instantiate Running
-        
+        string date = "03 Nov 2022";
+        float length = 30.0F;
+        float distance = 3.0F;
+        Running running = new Running(date, length, distance);
+        string summary = running.GetSummary();
+        Console.WriteLine(summary);
 
         // Instantiate Biking
 
